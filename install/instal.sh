547b89bd13692f6798466114eaf40dd2b48fdd5c
NTUMBABU <tshiteyababci@gmail.com>
2020-09-22 14:56:06 +0400

    edition du fichier install

---
+++ install/instal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#clonage du script 
+#git clone https://github.com/NTUMBABU/python_crypt.git
+
+#creation de dossier
+mkdir ~/cryptoProject/
+mkdir ~/cryptoProject/toCrypt/
+mkdir ~/cryptoProject/crypted/
+mkdir ~/cryptoProject/toDecrypt/
+mkdir ~/cryptoProject/decrypted/
+
