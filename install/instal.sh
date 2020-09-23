050dcd0cb50f7a04465e0a06a076dfbca0f7d8f9
NTUMBABU <tshiteyababci@gmail.com>
2020-09-23 06:59:24 +0400

    edition des fichier service

--- install/instal.sh
+++ install/instal.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+#creation du dossier soource
+mkdir ~/Dossier_source/
+
 #clonage du script 
-#git clone https://github.com/NTUMBABU/python_crypt.git
+git clone https://github.com/NTUMBABU/python_crypt.git ~/Dossier_source
+
+#copie des fichier source dans le dossier systmd
+cp ~/Dossier_source/source/mon_service.service /etc/systemd/system/
+cp ~/Dossier_source/source/mon_service_2.service /etc/systemd/system/
+
+#Activation des service
+systemctl enable mon_service.service
+systemctl enable mon_service_2.service
 
 #creation de dossier
 mkdir ~/cryptoProject/
 mkdir ~/cryptoProject/toCrypt/
-mkdir ~/cryptoProject/crypted/
+mkdir ~/cryptoProject/crypted/ 
 mkdir ~/cryptoProject/toDecrypt/
 mkdir ~/cryptoProject/decrypted/
 
